05020991c99e8a213ad0d59eec7ef7842250a93e
petroxsz <rogerio.souza@escolar.ifrn.edu.br>
2026-02-01 18:15:35 -0300

    vai

--- lua sangrenta/Assets/scripts/MenuUI.cs
+++ lua sangrenta/Assets/scripts/MenuUI.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Jogo fechado");
     }
-    public void Teste()
-{
-    Debug.Log("FUNCIONOU");
-}
-
 }
